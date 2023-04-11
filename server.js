10f4ab235f7eb7eb77c133370d69a6d208623478
harshnambiar <harrisonlvl2@gmail.com>
2023-04-11 12:27:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const app = express();
+
+require('dotenv').config({
+  path: './config/config.env',
+});
+
+// Connect to DB
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+// cors to allow to deal with frontend locally
+if (process.env.NODE_ENV === 'development') {
+  // morgan is used for logging
+  app.use(morgan('dev'));
+}
+
+// Load Routes
+//const authRouter = require('./routes/auth.route');
+const userRouter = require('./routes/user.route');
+const taskRouter = require('./routes/task.route');
+const listRouter = require('./routes/list.route');
+const taskEntryRouter = require('./routes/taskEntries.route');
+const projectRouter = require('./routes/project.route');
+const broadcastRouter = require('./routes/broadcast.route');
+const entriesRouter = require('./routes/entries.route');
+const messageRouter = require('./routes/messages.route');
+
+// app.use('/api/', authRouter);
+app.use('/api/', userRouter);
+app.use('/api/', taskRouter);
+app.use('/api/', listRouter);
+app.use('/api/', taskEntryRouter);
+app.use('/api/', projectRouter);
+app.use('/api/', broadcastRouter);
+app.use('/api/', entriesRouter);
+app.use('/api/', messageRouter);
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'Page Not Found',
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log(`Server running on port ${PORT} 🔥`));
+
+// TODO
+
+// 5.) Translate
